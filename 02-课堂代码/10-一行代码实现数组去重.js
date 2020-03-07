21bfd00633186763aa373f5f0fbc4908bf51724f
wuhu <wuhu@itcast.cn>
2020-03-07 12:03:22 +0800

    笔记整理

--- 02-课堂代码/10-一行代码实现数组去重.js
+++ 02-课堂代码/10-一行代码实现数组去重.js
@@ -1,5 +1,9 @@
 
-// 需求：实现数组去重。
+// 需求：实现数组去重。（面试题）
+
+// 知识点：
+//      1. new Set()
+//      2. 展开运算符
 // let arr = [11, 22, 33, 11, 22, 33];
 // const s1 = new Set(arr);  // 把数组转成 Set 对象实例
 // arr = [...s1];     // 把 Set 对象实例展开变成数组
@@ -7,21 +11,25 @@
 // console.log(arr);   // [ 11, 22, 33 ]  实现数组去重
 
 
+// 把上面代码写成一行。
 // let arr = [11, 22, 33, 11, 22, 33];
 // // 一行代码实现数组去重
 // arr = [...new Set(arr)];
 // console.log(arr);
 
 
-// 封装：arrayUnique  封装一个方法实现数组去重。
+// 最终封装：arrayUnique  封装一个方法实现数组去重。
 // 由于 arr 需要重新赋值，所以用 let 声明变量
 let arr = [11, 22, 33, 11, 22, 33];
-// 函数极限封装：形参只有一个所以省略了()，函数体内只有一句代码并且就是返回值，同时省略了 {} 和 return
+// 函数极限封装：
+//      1. 形参只有一个所以省略了()
+//      2. 函数体内只有一句代码并且就是返回值，同时省略了 {} 和 return
 const arrayUnique = array => [...new Set(array)];
 // 功能等价于：(没化简前)
 // const arrayUnique = (array) => {
 //     return [...new Set(array)];
 // }
+
 // 调用方法实现去重，再赋值给自己。
 arr = arrayUnique(arr);
 console.log(arr);   // [ 11, 22, 33 ]
